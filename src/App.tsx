397011046998c5a2b45c879dbc058da08c8d87ae
mteplicki <261726@student.pwr.edu.pl>
2024-01-17 14:59:55 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { createSignal } from 'solid-js'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline hover:text-blue-950">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
